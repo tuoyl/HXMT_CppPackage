e1b4548464ce44632ea2d601c793919c067f58c7
Youli <youlituo@gmail.com>
2019-04-15 22:29:40 +0800

    Add files via upload

---
+++ addspec.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string.h>
+#include "fitsio.h"
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+void PrintError(int status);
+void ReadSpecFile(char* filename, double counts, double exposure, int nRows, char telescop, char instrume, int detchans);
+
+void help()
+{
+        cout<<endl<<"USAGE"<<endl
+        <<"addspec: just use for adding the PHA spectrum of same detID!"<<endl
+        <<"addspec inputPHA.txt inputBKG.txt inputRSP.txt spec.fits bkg.fits totalRSP.rsp"<<endl;
+}
+
+
+int main(int argc, char* argv[])
+{
+    std::ifstream infilelist;
+    infilelist.open(argv[1]);
+    char filename[200];
+
+    int ifile = 0;
+    while(infilelist.getline(filename, sizeof(filename)))
+    {
+        std::cout << filename << endl;
+        double counts;
+        double exposure;
+        int nRows;
+        char telescop;
+        char instrume;
+        int detchans;
+        ReadSpecFile(filename, counts, exposure, nRows, telescop, instrume, detchans);
+    }
+
+    return 0;
+}
+
+void ReadSpecFile(char* filename, double counts, double exposure, int nRows, char telescop, char instrume, int detchans)
+{
+    fitsfile* fptr;
+    char card[FLEN_CARD];
+    int status = 0, datatype, anynull;
+    if(fits_open_file(&fptr, filename, READONLY, &status)) PrintError(status);
+    if(ffmahd(fptr, 2, &datatype, &status)) PrintError(status);
+    if(ffgky(fptr, TDOUBLE, "EXPOSURE", &exposure, NULL, &status)) PrintError(status);
+    if(ffgky(fptr, TINT, "NAXIS2", &nRows, NULL, &status)) PrintError(status);
+    if(ffgky(fptr, TSTRING, "TELESCOP", &telescop, NULL, &status)) PrintError(status);
+    if(ffgky(fptr, TSTRING, "INSTRUME", &instrume, NULL, &status)) PrintError(status);
+    if(ffgky(fptr, TINT, "DETCHANS", &detchans, NULL, &status)) PrintError(status);
+
+    for (int currentRow = 1; currentRow <= nRows; currentRow++)
+    {
+        if(fits_read_col_dbl(fptr, 2, currentRow, 1, 1, 0.0, &counts, &anynull, &status)) PrintError(status);
+        if(fits_close_file(fptr, &status)) PrintError(status);
+    }
+
+
+}
+
+void WriteSpecFile()
+{
+
+}
+
+void ReadRspFile()
+{
+}
+
+void WriteRspFile()
+{
+
+}
+
+void PrintError(int status)
+{
+    if (status)
+    {
+        fits_report_error(stderr, status);
+        exit(status);
+    }
+    return;
+}
+
+
+
+
+
