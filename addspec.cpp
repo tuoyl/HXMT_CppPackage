f355099632b5a1a0516fc245b8e548f14ecb9f99
Youli Tuo <crazuni1993@gmail.com>
2019-04-16 15:12:01 +0800

    initial data size done

--- addspec.cpp
+++ addspec.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 void PrintError(int status);
-void ReadSpecFile(char* filename, double counts, double exposure, int nRows, char telescop, char instrume, int detchans);
+void ReadSpecFile(char* filename, double* &counts, double exposure, int nRows, char telescop, char instrume, int detchans);
+int InitDataSize(char* filename);
 
 void help()
 {
@@ -17,33 +18,69 @@ void help()
 }
 
 
+
 int main(int argc, char* argv[])
 {
+    /* Pass Spec file list */
     std::ifstream infilelist;
     infilelist.open(argv[1]);
     char filename[200];
-
     int ifile = 0;
+    double* total_counts;
+    double total_exposure = 0;
+    int counts_size;
+
     while(infilelist.getline(filename, sizeof(filename)))
     {
         std::cout << filename << endl;
-        double counts;
+        double* counts;
         double exposure;
         int nRows;
         char telescop;
         char instrume;
         int detchans;
+
+        /*initial data size*/
+        if (total_counts == NULL)
+        {
+            counts_size = InitDataSize(filename);
+            total_counts = new double[counts_size];
+        }
+
+        cout << "size of counts" << sizeof(counts) << endl;
         ReadSpecFile(filename, counts, exposure, nRows, telescop, instrume, detchans);
+        total_exposure = total_exposure + exposure;
+        cout << "size of counts" << sizeof(counts) << endl;
+        for (int i=0; i <= nRows; i++)
+        {
+//            total_counts[i] = total_counts[i] + counts[i];
+        }
+
     }
 
     return 0;
 }
 
-void ReadSpecFile(char* filename, double counts, double exposure, int nRows, char telescop, char instrume, int detchans)
+int InitDataSize(char* filename)
+{
+    int sizenum; 
+
+    fitsfile* fptr;
+    char card[FLEN_CARD];
+    int status = 0, datatype, anynull;
+    if(fits_open_file(&fptr, filename, READONLY, &status)) PrintError(status);
+    if(ffmahd(fptr, 2, &datatype, &status)) PrintError(status);
+    if(ffgky(fptr, TINT, "NAXIS2", &sizenum, NULL, &status)) PrintError(status);
+    if(fits_close_file(fptr, &status)) PrintError(status);
+    return sizenum;
+}
+
+void ReadSpecFile(char* filename, double* &counts, double exposure, int nRows, char telescop, char instrume, int detchans)
 {
     fitsfile* fptr;
     char card[FLEN_CARD];
     int status = 0, datatype, anynull;
+    double doublenull = 0;
     int colnum_COUNTS;
 
     if(fits_open_file(&fptr, filename, READONLY, &status)) PrintError(status);
@@ -55,11 +92,9 @@ void ReadSpecFile(char* filename, double counts, double exposure, int nRows, cha
     if(ffgky(fptr, TINT, "DETCHANS", &detchans, NULL, &status)) PrintError(status);
     if(fits_get_colnum(fptr, CASEINSEN, "COUNTS", &colnum_COUNTS, &status)) PrintError(status);
 
-    for (int currentRow = 1; currentRow <= nRows; currentRow++)
-    {
-        if(fits_read_col_dbl(fptr, colnum_COUNTS, currentRow, 1, 1, 0.0, &counts, &anynull, &status)) PrintError(status);
+    counts = new double[nRows];
 
-    }
+    if(fits_read_col(fptr, TDOUBLE, colnum_COUNTS, 1, 1, nRows, &doublenull, &counts, &anynull, &status)) PrintError(status);
 
     if(fits_close_file(fptr, &status)) PrintError(status);
 }
@@ -71,6 +106,7 @@ void WriteSpecFile()
 
 void ReadRspFile(char* filename, double* energ_lo, double* energ_hi, int N_GRP, int F_CHAN, int detchans, double* matrix, int matrix_nRows)
 {
+    cout << "RSP file: " << filename << endl;
     fitsfile* fptr;
     char card[FLEN_CARD];
     int status = 0, datatype, anynull;
